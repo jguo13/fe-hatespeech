87e13dba2c12e2c23cececda74b5586203c95274
kguo <kguo2010@gmail.com>
2021-03-22 22:38:02 -0400

    add spinner

--- src/components/HateCard.vue
+++ src/components/HateCard.vue
@@ -1,19 +1,40 @@
 <template>
   <div>
-    <b-card
-    header="Hate Speech"
-    header-text-variant="white"
-    header-tag="header"
-    header-bg-variant="dark"
-    footer="Card Footer"
-    footer-tag="footer"
-    footer-bg-variant="success"
-    footer-border-variant="dark"
-    title="Percentage"
-    style="max-width: 20rem;"
-    >
-    <b-card-text>{{ percentage }}</b-card-text>
-  </b-card>
+
+    <b-jumbotron>
+      <template #header>Asian Hate Speech</template>
+
+      <template #lead>
+        This is a simple hero statistic that tells us the percentage of hate speech on 4chan
+      </template>
+
+      <hr class="my-4">
+
+      <p>
+        It uses high level machine learning.
+      </p>
+
+      <b-card
+      header="Asian Hate Speech"
+      header-text-variant="white"
+      header-tag="header"
+      header-bg-variant="dark"
+      footer="Data By Joyce"
+      footer-tag="footer"
+      footer-bg-variant="success"
+      footer-border-variant="dark"
+      title="Percentage"
+      style="max-width: 20rem;"
+      >
+      <b-spinner v-if="loadingStatus" variant="primary" label="Spinning"></b-spinner>
+      <b-card-text v-else>
+        {{ percentage }}
+      </b-card-text>
+    </b-card>
+
+
+    <b-button variant="primary" href="#">Update Data</b-button>
+  </b-jumbotron>
 </div>
 </template>
 
@@ -23,6 +44,7 @@
     name: 'HateCard',
     data() {
       return {
+        loadingStatus: true,
         percentage: 0.0
       }
     },
@@ -31,13 +53,17 @@
     },
     methods: {
       getHateSpeechPercentage() {
-        const path = 'http://127.0.0.1:5000/books';
+        this.loadingStatus = true;
+        const path = 'http://127.0.0.1:5000/get_hate';
         axios.get(path)
         .then((res) => {
           this.percentage = res.data.percentage;
+          this.loadingStatus = false;
         })
         .catch((error) => {
           console.error(error);
+          this.percentage = -1.0;
+          this.loadingStatus = false;
         });
       }
     }
